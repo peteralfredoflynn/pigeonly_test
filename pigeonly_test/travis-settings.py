926673cea2e5bfbe5d1ad13bd4123cae6dfa9972
Peter Flynn <fredoflynn@gmail.com>
2016-06-08 06:15:52 -0700

    modifiy travis settings

--- pigeonly_test/travis-settings.py
+++ pigeonly_test/travis-settings.py
@@ -2,4 +2,15 @@ from pigeonly_test.settings import *
 
 SECRET_KEY = 'vwn2!7ow-@azz(l#69k15&b15#)w4%b1f62m_8o&*s!clw(tk$'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pigeonly_test',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': ''
+    }
+}
+
 LOGGING = None
